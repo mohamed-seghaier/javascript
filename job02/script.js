d3e940e8e1aa155a0b5c7716039132f70dde6974
mohamed-seghaier <91876112+mohamed-seghaier@users.noreply.github.com>
2021-10-18 15:04:49 +0200

    job 2 done
    Modification des dossiers pour y intégrer les dossiers job01 et job02

---
+++ job02/script.js
@@ -0,0 +1,7 @@
+function afficherjourssemaines() {
+    let jourssemaines = new Array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+
+    for (let idx = 0; jourssemaines[idx]; idx += 1)
+        console.log(jourssemaines[idx]);
+
+}
