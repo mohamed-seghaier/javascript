b104eb4a6dec5a77a446ba3c4a59db0e30714deb
mohamed-seghaier <91876112+mohamed-seghaier@users.noreply.github.com>
2021-10-18 15:20:25 +0200

    job3
    Job 3 done

---
+++ job03/script.js
@@ -0,0 +1,6 @@
+function citation() {
+    if (document.getElementById("citation") != null) {
+        let citation = document.getElementById('citation').innerText;
+        console.log(citation)
+    }
+}
