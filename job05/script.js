9c360e0de5f0c228954cc8068874faa6f5173812
mohamed-seghaier <91876112+mohamed-seghaier@users.noreply.github.com>
2021-10-19 11:26:50 +0200

    job04
    correction mauvaise compréhension sujet

--- job05/script.js
+++ job05/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('keyup', (e) => {
     let myElement = document.getElementById("keylogger");
-    if (myElement === document.activeElement) {} else {
+    if (myElement === document.activeElement) {
         myElement.value += e.key;
+
+    } else {
         myElement.value += e.key;
     }
 
